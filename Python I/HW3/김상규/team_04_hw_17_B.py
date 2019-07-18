0fd8e0ca953fa50bdda1481ef6af6613ac97d762
sangqkim <35060845+sangqkim@users.noreply.github.com>
2019-07-18 17:22:26 +0900

    Update team_04_hw_17_B.py

--- Python I/HW3/김상규/team_04_hw_17_B.py
+++ Python I/HW3/김상규/team_04_hw_17_B.py
@@ -71,3 +71,46 @@ your_dog.sit()
                
 
 
+# 9-2
+"""A class that can be used to represent a care."""
+
+class Car():
+    """A simple attempt to represent a car."""
+    def __init__(self, manufacturer, model, year):
+        self.manufacturer = manufacturer
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+    
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_nmae = str(self.year) + ' ' + self.manufacturer + '' + self.model
+        return long_nmae.title()
+    
+    def read_odometer(self):
+        """Print a statement showing the car's mileage."""
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+        
+    def update_odometer(self, mileage):
+        """
+        Set the odometer reading to the given value.
+        Reject the change if it attempts to roll the dodometer back.
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+    
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
+
+my_used_car = Car('subaru', 'outback', 2013)
+print(my_used_car.get_descriptive_name())
+my_used_car.update_odometer(23500)
+my_used_car.read_odometer()
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
+
+
+# 
