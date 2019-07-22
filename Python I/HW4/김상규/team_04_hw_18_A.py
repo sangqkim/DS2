c33f99b298c34e5c019ef41b03cb29c184343db5
sangqkim <35060845+sangqkim@users.noreply.github.com>
2019-07-23 00:07:44 +0900

    Update team_04_hw_18_A.py

--- Python I/HW4/김상규/team_04_hw_18_A.py
+++ Python I/HW4/김상규/team_04_hw_18_A.py
@@ -62,25 +62,133 @@ print(pyt.getDistance())
 
 
 
-
+print("*****")
 # 5
-#class ProfessorStudent:
-#    def __init__(self):
-#        pass
-#        
-#    def getName(self):
-#        pass
-#    def getDepart():
-#        pass
-#    def getCredit():
-#        pass
-#    def setCredit():
-#        pass
-#    def increaseYear():
-#        pass
-#    def getCourse():
-#        pass
-#    def getAnnualSalary():
-#        pass
-#    def raiseSalary:
-#        
+class Person:
+    def __init__(self, name):
+        self.name = name
+        self.year = 0
+        
+    # 빈 칸을 채워주는 method 만듬 setXXX()
+    def setName(self, name):
+        self.name = name
+        
+    def setDepart(self, depart):
+        self.depart = depart
+        
+    def setCredit(self, credit):
+        self.credit = credit
+        
+    def setCourse(self, course):
+        self.course = course    
+        
+    def setSalary(self, salary):
+        self.salary = salary
+    
+    def raiseSalary(self, percent):
+        self.salary = self.salary * (1+percent/100)
+        return self.salary
+#        print(year, " year salary :", percent, "% raise: ", self.total_salary) 
+        
+    
+    def getTotalSalary(self, year, percent):
+        
+        if percent == 0:
+            self.increaseYear()
+            self.total_salary = self.salary*year
+            print(year, " year salary: ", self.total_salary)
+            
+        else:
+            total = 0
+            for i in range(year):
+                self.increaseYear()
+                total += self.raiseSalary(percent)                                    
+            self.total_salary = total
+            print(year, " year salary :", percent, "% raise: ", self.total_salary)           
+        
+    def increaseYear(self):
+        self.year += 1  
+    
+    def getAnnualSalary(self, salary):
+        self.salary = salary * 12        
+   
+
+class Professor(Person):
+    def __init__(self, name):
+        super().__init__(name)
+
+class Student(Person):
+    def __init__(self, name):
+        super().__init__(name)
+        
+
+# Create an object called tim_cook 
+tim_cook = Professor('Tim Cook')
+
+# Set tim_cook values to match the data from the table 
+tim_cook.setCourse('Soft.Arch.')
+tim_cook.setDepart('CSE')
+tim_cook.setSalary(5500)
+
+# Write a code that will calculate the total amount of salary tim_cook will receive in five years
+tim_cook.getTotalSalary(5, 0)
+
+# Write a code that will calculate the total amount of salary of tim_cook if his salary is raised 15% every year for five year 
+tim_cook.getTotalSalary(5, 15)
+
+
+
+print("*****")
+# 6
+class Person:
+    
+    def __init__(self, name):
+        self.name = name
+  
+    def setAddress(self, address):
+       self.address = address
+       
+    def setSchool(self, school):
+        self.school = school
+    
+    def setAnnualPay(self, pay):
+        self.pay = pay
+    
+    def raiseAnnualPay(self, percent):
+        self.pay = self.pay * (1+percent/100)
+        return self.pay
+    
+    def getAnnualSalary(self, year, percent):
+        for i in range(year):
+            self.raiseAnnualPay(percent)
+            
+
+class Staff(Person):
+    def __init__(self, name):
+        super().__init__(name)
+
+class Student(Person):
+    def __init__(self, name):
+        super().__init__(name)
+        
+# Create an object called tom and dane 
+tom = Staff('tom'.title())
+dane = Staff('dane'.title())
+
+#  Set tom and dane values to match the data from the table 
+tom.setAddress('Gangnam')
+tom.setSchool('Yonsei')
+tom.setAnnualPay(35000)
+
+dane.setAddress('Shindorim')
+dane.setSchool('Sogang')
+dane.setAnnualPay(20000)
+
+# Write a code that will calculate tom and dane annual salary after seven year
+tom.getAnnualSalary(7, 7)
+dane.getAnnualSalary(7, 15)
+#print(tom.pay)
+#print(dane.pay)
+
+# Write a code to see who has a larger monthly pay 
+# 여기 해야함.
