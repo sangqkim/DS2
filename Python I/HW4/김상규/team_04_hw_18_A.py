44e4686e19dd4e8fb1a8e53feb337fe71694228e
sangqkim <35060845+sangqkim@users.noreply.github.com>
2019-07-24 15:40:54 +0900

    Add files via upload

--- Python I/HW4/김상규/team_04_hw_18_A.py
+++ Python I/HW4/김상규/team_04_hw_18_A.py
@@ -1,4 +1,5 @@
 # 1
+print("***** #1 *****")
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -39,65 +40,68 @@ print(pyt.getDistance())
 
 
 
-print("*****")
+print()
+print("***** #2 *****")
 # 2
 class Calculator:  
     
     def __init__(self):
         self.total = 0
         self.calc_str = []
+        self.history = []
+        self.n = 0
         
     def add(self, num):
         self.total += num
+        self.n += 1
         if len(self.calc_str) == 0:
             self.calc_str.append(str(num))
         else:
             self.calc_str.append('+ ' + str(num))
-
     
     def subtract(self, num):
         self.total -= num
-        self.calc_str.append('- ' + str(num))
-        
-
+        self.n += 1
+        self.calc_str.append('- ' + str(num))     
         
     def multiply(self, num):
         self.total *= num
+        self.n += 1
         self.calc_str.append('* ' + str(num))
-#        self.calc_str.append('*')
-#        self.calc_num.append(num)
     
     def equals(self, equal=False): 
-        if equal == True:
+        if self.n != 0:            
             self.calc_str.append('= ' + str(self.total))
-            self.showHistory()
-#        
+            self.history.append(self.calc_str)
+            self.calc_str = []
+            self.temp = self.total
+            self.total = 0
+            if equal == True:
+                print(self.temp)           
     
     def showHistory(self):
-        if len(self.calc_str) == 0:
+        if self.n == 0:
             print("No calculation done yet!") 
             print("History:")
         else:
             print("History:")
-            for i in range(len(self.calc_str)):
-                print(self.calc_str[i], end=' ')
-            print('= ', self.total)
-        self.calc_str = []
-            
-#       
-        
-test = Calculator()
-#test.equals()
-#test.showHistory()
-#
-#test.add(2)
-#test.subtract(1)
-#test.equals()
-#test.showHistory()
+            for i in range(len(self.history)):
+                for j in range(len(self.history[i])):
+                    print(self.history[i][j], end=' ')
+                print()
 
-#test.add(2)
-#test.multiply(4)
-#test.equals(True)
+test = Calculator()
+test.equals()
+test.showHistory()
+
+test.add(2)
+test.subtract(1)
+test.equals()
+test.showHistory()
+
+test.add(2)
+test.multiply(4)
+test.equals(True)
 
 test.add(10)
 test.subtract(5)
@@ -105,30 +109,110 @@ test.multiply(2)
 test.equals()
 test.showHistory()
 
-#print(test.calc_str)
-#print(test.calc_num)
-#print(test.total)   
-
-print("*****")
+   
+print()
+print("***** #3 *****")
 # 3
-#class Account:
-#    def __init__(self, name):
-#        self.name = name
-#        self.transaction = []
-#        self.deposit_value = 0
-#    
-#    def deposit(self, value):
-#        self.depositve_value = value
-#        self.transaction_log('deposit', value)
-#    def transaction_log(self, keyword, value):
-#        self.transaction_log(keyword, value)
+class Account:
+    def __init__(self, account_holder):
+        self.balance = 0
+        self.holder = account_holder
+        self.transactions = []
+        
+    def deposit(self, amount):
+        self.balance += amount
+        self.transactions.append(('deposit', amount))
+    
+    def withdrawal(self, amount):
+        
+        if amount > self.balance:
+            return "Insufficient funds"
+        self.balance = self.balance - amount
+        self.transactions.append(('withdrawal', amount))
+            
+    def status(self):
+        print(self.holder + ": ", end=" ")
+        print(self.transactions)
+        
+    
+def main():
+    bob_account = Account('Bob')
+    bob_account.deposit(1000000)
+    bob_account.withdrawal(100)
+    bob_account.deposit(440)
+    bob_account.status()
+    
+    tom_account = Account('Tom')
+    tom_account.deposit(5000000)
+    tom_account.withdrawal(250)
+    tom_account.deposit(875)
+    tom_account.status()
+
+main()
+
+print()
+print("***** #4 *****")
+# 4
+Atno_to_Symbol = {1:'H', 2:'He', 3:'Li', 4:'Be', 5: 'B', 6:'C', 7: 'N', 8:'O'}
+class atom(object):
+    def __init__(self, atno, x, y, z):
+        self.atno = atno
+        self.position = (x, y, z)
+        
+    def symbol(self): # class method
+        return Atno_to_Symbol[self.atno]
+    
+    def __repr__(self): # overloads printing
+        return '%d %10.4f %10.4f %10.4f' %(self.atno, self.position[0], self.position[1], self.position[2])
+
+class molecule:
+    def __init__(self, name='Generic'):
+        self.name = name
+        self.atomlist = []
+    
+    def addatom(self, atom):
+        self.atomlist.append(atom)
+        
+    def __repr__(self):
+        str = "This is a molecule named %s\n" % self.name
+        str = str + 'It has %d atoms\n' % len(self.atomlist)
+        for atom in self.atomlist:
+            str = str + "%s \n" % atom
+        return str
+''' 코드 설명
+Atno_to_Symbol은 원자 번호와 원자를 key-value pair를 갖는 dictionary 이다.
+atom class
+__init__ method는 클래스를 초기화시키면서 initial value를 받는다.
+symbol method는 self.atno에 맞는 Atno_to_Symbol의 value (원자기호)를 return 해준다.
+__repr__ method는 atom 클래스로부터 생성된 object를 print할 때 출력되는 내용을 정의한다.
+출력 내용은 class 초기화 때 입력받은 self.atno와 x,y,z 값을 나타낸다.
+
+molecule class
+__init__ method는 클래스를 초기화시키면서 name이라는 초기값는 받는데, 
+default는 'Generic'이고, self.atomlist라는 빈 리스트를 생성한다.
+addatom method는 atom object를 인수로 받는다.
+__repr__ method는 str에 입력된 문장 출력과 함께 atom object에서 return된 값을 출력한다.
+
+'''
+
+at = atom(6, 0.0, 1.0, 2.0)
+print(at)
+at.symbol()
+
+
+mol = molecule('Water')
+at = atom(8, 0., 0. ,0.)
+mol.addatom(at)
+mol.addatom(atom(1, 0., 0., 1.))
+mol.addatom(atom(1, 0., 0., 0.))
+print(mol)
 
 
 
 
 
-
-print("*****")
+print()
+print("***** #5 *****")
 # 5
 class Person:
     def __init__(self, name):
@@ -204,7 +288,8 @@ tim_cook.getTotalSalary(5, 15)
 
 
 
-print("*****")
+print()
+print("***** #6 *****")
 # 6
 class Person:
     
