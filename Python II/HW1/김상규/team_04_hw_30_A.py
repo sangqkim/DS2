9fc85d67ebccb345fb89290f066c9044ffd8d0dd
sangqkim <35060845+sangqkim@users.noreply.github.com>
2019-08-03 13:57:08 +0900

    Add files via upload

---
+++ Python II/HW1/김상규/team_04_hw_30_A.py
@@ -0,0 +1,55 @@
+#1
+import numpy as np
+
+a = np.zeros(10)
+#np.array([0 for _ in range(10)])
+print("(1)")
+print(a)
+
+#2
+a[4] = 1
+print("(2)")
+print(a)
+
+#3
+a = np.arange(10, 50)
+print("(3)")
+print(*a, end=' ')
+
+#4
+a = np.arange(0, 25)
+a = a.reshape(5,5)
+#a = np.arange(0, 25).reshape(5,5)
+print("(4)")
+print(a)
+
+#5
+a = np.eye(5)
+print("(5)")
+print(a)
+
+#6
+np.random.seed(5)
+a = np.random.random((5,5))
+print("(6)")
+print(a)
+print(a.min())
+print(a.max())
+
+#7
+a = np.ones((4,3))
+b = np.random.random((3,2))
+c = np.dot(a,b)
+print(a)
+print(b)
+print(c)
+
+#8
+print(c.T)
+print(c.transpose())
+
+#9
+a = np.arange(25).reshape(5,5)
+b = np.arange(25,50).reshape(5,5)
+c = a+b
+d = a-b
---
+++ Python II/HW1/김상규/team_04_hw_31_A.py
@@ -0,0 +1,65 @@
+# (1)
+import pandas as pd
+import numpy as np
+
+exam_data = {'name': ['Anastasia', 'Catherine', 'Cahill', 'James', 'Emily', 'Michael', 'Monica',
+                      'Laura', 'Kevin', 'Jordan'],
+            'score': [13, 9.5, 16.5, np.nan, 11, 20, 17, np.nan, 8.5, 19],
+            'attempts': [1, 3, 3, 2, 2, 3, 2, 3, 2, 1],
+            'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+df = pd.DataFrame(exam_data, index=labels)
+
+# (1-1)
+print(df[['name', 'score']]) # 여러 개 컬럼 추출하 때는 list 타입
+
+# (1-2)
+print(df.iloc[:3])
+
+# (1-3)
+print(df[['name', 'score']].iloc[[0,1,4,5]])
+
+# (1-4)
+print(df[df['attempts'] > 2]) # 안에가 true인 컬럼 추출
+
+
+
+# (2)
+# (2-1)
+print(df[df['score'].isnull()])
+
+# (2-2)
+print(df[(df['attempts']<2) & (df['score']>15)])
+
+# (2-3)
+print(df['attempts'].sum())
+
+# (2-4)
+print(df['score'].mean())
+
+
+# (3)
+# (3-1)
+labels.append('k')
+df.loc['k'] = ['Saya', 17.5, 2, 'yes']
+
+# (3-2)
+df = df.drop('k', axis=0)
+
+# (3-3)
+df2 = df.drop('attempts', axis=1)
+
+# (3-4)
+df.groupby('attempts').sum()
+
+
+#4
+exam2_data = {'name': ['Anastasia', 'Catherine', 'Ronaldo', 'James', 'Messi', 'Michael', 'Monica',
+                      'Laura', 'Klassen', 'Jonas'],
+            'score2': [11, 20, 16.5, np.nan, 10, 15, 20, np.nan, 8, 8]}
+labels2 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df2 = pd.DataFrame(exam2_data, index=labels2)
+# pd.merge(df, df2, on='name') # 사람의 이름이 동일하므로 name을 기준으로 d
+inner_join = pd.merge(df, df2, on='name')
+print(inner_join)
