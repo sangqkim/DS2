760edc21afc836f423b537eb168d7ef3124109f0
chikison <wowzero@naver.com>
2019-08-04 12:20:48 +0900

    update

--- Python2/HW1/25.py
+++
@@ -1,8 +0,0 @@
-A = Digraph
-A.attr('node', color='goldenrod2', style='filled', size='7.5')
-A.edge('7th Edition', '32V')
-'''
-'''
-A.render('7th edition(' + str(engine), + '_engine)')
-
-g = Digraph(comment = 'Red-Block Tree')
