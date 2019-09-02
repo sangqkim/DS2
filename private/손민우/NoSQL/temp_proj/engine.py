68f86796f235886efda70fb65f2775502ae46366
chikison <wowzero@naver.com>
2019-09-02 19:43:18 +0900

    update

--- private/손민우/NoSQL/temp_proj/engine.py
+++ private/손민우/NoSQL/temp_proj/engine.py
@@ -6,29 +6,51 @@ client = MongoClient()
 db = client.ds2
 enron = db.emails
 
-raw_input = sys.argv[1]
-
-# TODO:
+# raw_input = sys.argv[1]
+raw_input = "Social / not: Network / sort: date"
+# raw_input = "from: robyn@layfam.com"
+# raw_input = "to: cindy.olson@enron.com, greg.whalley@enron.com / Please / not: Attached, previously"
 # fill in the blank
-enron.create_index([('subject',TEXT), ('text', TEXT)], {'weights':{'subject':2, 'text':1}})
+enron.create_index([('subject',TEXT), ('text', TEXT)], weights = {'subject':2, 'text':1})
 s = raw_input.split('/')
-di = dict()
+lnot, lfrom, lto, lsearch, lsort = [], [], [], [], []
+
 for i in s:
-    if ':' in i:
-        cmd, ss = i.strip().split(':')
-        if cmd == 'not':
-            di.update(enron.find({'subject':{'$nin':[ss]}}))
-        elif cmd == 'from':
-            di.update(enron.find({'sender':ss}))
-        elif cmd == 'to':
-            di.update(enron.find({'to':ss}))
-        elif cmd == 'sort':
-            sort = ss
+    cmd = i.replace(" ", "").split(':')
+    if cmd[0] == 'not':
+        lnot.extend(cmd[1].split(','))
+    elif cmd[0] == 'from':
+        lfrom.append(cmd[1])
+    elif cmd[0] == 'to':
+        lto.extend(cmd[1].split(','))
+    elif cmd[0] == 'sort':
+        lsort.append(cmd[1])
     else:
-        searcher = i
-        enron.find({'subject':{'$in':[searcher]}})
+        lsearch.extend(cmd[0].split(','))
 
+query = []
+if lnot or lsearch:
+    text = ''
+    for i in lsearch:
+        text = text + ' ' + str(i)
+    for i in lnot:
+        text = text + ' -' + str(i)
+    search = {'$match': {'$text': {'$search': text, '$caseSensitive': False, '$language': 'en'}}}
+    query.append(search)
+if lto:
+    to = {'$match': {'to': {'$in': lto}}}
+    query.append(to)
+if lfrom:
+    sender = {'$match': {'sender': lfrom[0]}}
+    query.append(sender)
+if lsort:
+    if lsort[0] == 'score':
+        sort = {'$sort': {'score': {'$meta': 'textScore'}}}
+    elif lsort[0] == 'date':
+        sort = {'$sort': {'date': -1}}
+    query.append(sort)
 
+result = enron.aggregate(query)
 print('sender\t\t\tsubject\t\t\ttext\t\t\t\t\tdate')
 for item in result:
     print('{}\t{}\t{}\t{}'.format(
